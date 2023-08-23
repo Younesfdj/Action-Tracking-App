aa8f3b1608a24b213187366b1f76eed99753acdf
Younes Ferradji <113609638+Younesfdj@users.noreply.github.com>
2023-08-23 21:49:50 +0100

    Update StockList.jsx

--- src/components/StockList.jsx
+++ src/components/StockList.jsx
@@ -6,7 +6,6 @@ import { useNavigate } from "react-router-dom";
 
 export const StockList = () => {
   const [stockData, setStockData] = useState([]);
-  const [showRemove, setShowRemove] = useState(false);
   const { watchList, handleRemove } = useGlobalContext();
   const navigate = useNavigate();
 
